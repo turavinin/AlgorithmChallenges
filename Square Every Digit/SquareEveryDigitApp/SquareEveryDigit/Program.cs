d18cb9b077487392dff77bff534da004985e03f4
turavinin <aturavinin@gmail.com>
2021-01-24 11:31:36 -0300

    Add project files.

---
+++ Square Every Digit/SquareEveryDigitApp/SquareEveryDigit/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SquareEveryDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Kata.SquareDigits(9119);
+            Console.WriteLine(result);
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Kata
+    {
+        public static int SquareDigits(int n)
+        {
+            string outputString = "";
+
+            foreach (char ch in n.ToString())
+            {
+                var number = int.Parse(ch.ToString());
+                number = (int)Math.Pow(number, 2);
+
+                outputString += number.ToString();
+            }
+
+            var output = int.Parse(outputString);
+
+            return output;
+        }
+    }
+}
