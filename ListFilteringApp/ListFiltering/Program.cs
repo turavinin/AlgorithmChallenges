25ba136bb799f85129919b409af20186d7d14d6e
turavinin <aturavinin@gmail.com>
2021-02-01 07:26:12 -0300

    add best solution

--- ListFilteringApp/ListFiltering/Program.cs
+++ ListFilteringApp/ListFiltering/Program.cs
@@ -11,7 +11,18 @@ namespace ListFiltering
     // ListFilterer.GetIntegersFromList(new List<object>(){1, 2, "a", "b"}) => { 1, 2}
     // ListFilterer.GetIntegersFromList(new List<object>() { 1, 2, "a", "b", 0, 15 }) => { 1, 2, 0, 15}
     // ListFilterer.GetIntegersFromList(new List<object>() { 1, 2, "a", "b", "aasf", "1", "123", 231 }) => { 1, 2, 231}
-class Program
+
+    // BEST SOLUTION:
+
+    //public class ListFilterer
+    //{
+    //    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    //    {
+    //        return listOfItems.OfType<int>();
+    //    }
+    //}
+
+    class Program
     {
         static void Main(string[] args)
         {
