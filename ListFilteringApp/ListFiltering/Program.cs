0287cde57bb4cf7a5ed2fcd2ba2b8de5d0148081
turavinin <aturavinin@gmail.com>
2021-02-01 07:05:01 -0300

    start List Filtering

---
+++ ListFilteringApp/ListFiltering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListFiltering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
