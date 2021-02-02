cee5525149108cb618af2bbf1c86aa5e911da826
turavinin <aturavinin@gmail.com>
2021-02-02 08:22:53 -0300

    start challenge

---
+++ Dubstep/Dubstep/Dubstep/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Dubstep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dubstep.SongDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
+            
+        }
+
+        public class Dubstep
+        {
+            public static string SongDecoder(string input)
+            {
+                Regex regex = new Regex(@"WUB");
+
+                var splitedArr = regex.Split(input);
+                var output = string.Join("  ", splitedArr)
+
+                return output;
+
+            }
+        }
+    }
+}
