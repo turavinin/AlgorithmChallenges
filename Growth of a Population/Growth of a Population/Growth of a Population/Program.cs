15010e18d35f348d03261c8268653b206dca2612
turavinin <aturavinin@gmail.com>
2021-02-05 07:29:41 -0300

    comienzo algoritmo

---
+++ Growth of a Population/Growth of a Population/Growth of a Population/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Growth_of_a_Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            
+        }
+    }
+
+
+    class Arge
+    {
+
+        public static int NbYear(int p0, double percent, int aug, int p)
+        {
+            // your code
+        }
+    }
+}
